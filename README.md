39287848d4323fa66ddfcbf2862af046f90f820a
Gregor <gregor@radko.eu>
2017-06-24 17:57:25 +0200

    Added JWT-Security

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # spring-boot-test
+
+A simple Spring-Boot Demo Application
+
+## Usage
+
+There are 2 endpoints available `/tests` (GET) and `/testMethod` (POST).
+
+## Security
+
+To use the endpoints you need to auth first against `/login` (POST).
+After you can use the JWT as `Authentication` header.
---
+++ src/main/java/de/gregor/demo/security/AccountCredentials.java
@@ -0,0 +1,23 @@
+package de.gregor.demo.security;
+
+public class AccountCredentials {
+
+    private String username;
+    private String password;
+
+    String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
